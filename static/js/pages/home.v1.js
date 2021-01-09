516b094a8e14b69437331657dd90c19ab3735067
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-09 10:15:30 -0500

    a

--- static/js/pages/home.v1.js
+++ static/js/pages/home.v1.js
@@ -1,18 +1,19 @@
 
-    // load news
-    axios.get("/api/web/news?limit=4") // limit news
-        .then((res) => {
-            if (res.data.code !== 200)
-                return; // well that sucks 
 
-            let news = res.data.data
-            let first = news.shift()
+// load news
+axios.get("/api/web/news?limit=4") // limit news
+    .then((res) => {
+        if (res.data.code !== 200)
+            return; // well that sucks 
 
-            $("#news").append(newsFormat(first, true));
+        let news = res.data.data
+        let first = news.shift()
 
-            let base = `<br><article class="tile is-child" id="news"><div class="columns" id="secondaries">`
-            function newsFormat(data, first = false) {
-                return `<div class="column${first == false ? "" : " no-padding"} article">
+        $("#news").append(newsFormat(first, true));
+
+        let base = `<br><article class="tile is-child" id="news"><div class="columns" id="secondaries">`
+        function newsFormat(data, first = false) {
+            return `<div class="column${first == false ? "" : " no-padding"} article">
                     <a href="/news/${data.id}"><nav class="panel">
                         <p class="panel-heading news-header" style="background-image: url(${data.image_url || "/static/images/default_news.svg"});">
                         </p>
@@ -24,29 +25,29 @@
                         </div>
                     </nav></a>
                 </div>`
-            }
+        }
 
-            for (let newy of news) {
-                base += newsFormat(newy);
-            }
+        for (let newy of news) {
+            base += newsFormat(newy);
+        }
 
-            base += `</div></article>`;
-            $("#news").append(base);
-        });
+        base += `</div></article>`;
+        $("#news").append(base);
+    });
 
-    // load player count
-    axios.get("/api/metadata") // limit news
-        .then((res) => {
-            if (res.data.code !== 200)
-                return; // well that sucks 
+// load player count
+axios.get("/api/metadata") // limit news
+    .then((res) => {
+        if (res.data.code !== 200)
+            return; // well that sucks 
 
-            let data = res.data.data
+        let data = res.data.data
 
-            $("#total").html(` ${data.users.total} `);
-            $("#online").html(` ${data.users.online} `);
+        $("#total").html(` ${data.users.total} `);
+        $("#online").html(` ${data.users.online} `);
 
-            for (let key of Object.keys(data.records)) {
-                $(`#${key}-pp`).html(Math.floor(data.records[key].pp) + "pp");
-                $(`#${key}-holder`).html(data.records[key].holder);
-            }
-        });
+        for (let key of Object.keys(data.records)) {
+            $(`#${key}-pp`).html(Math.floor(data.records[key].pp) + "pp");
+            $(`#${key}-holder`).html(data.records[key].holder);
+        }
+    });
