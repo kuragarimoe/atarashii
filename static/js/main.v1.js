acfce1ccddb6c4e2249a24c8ecd791fd88751067
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-09 14:23:51 -0500

    sticky header

--- static/js/main.v1.js
+++ static/js/main.v1.js
@@ -1,3 +1,15 @@
+// sticky header
+window.onscroll = () => {
+    var header = document.getElementById("navbar");
+    var sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add("minimized");
+    } else {
+        header.classList.remove("minimized");
+    }
+}
+
 function makeError(message) {
     return `<div class="notification is-error"><button class="delete" onclick="$(this).parent().remove(); displayed_error = false;"></button><span id="err-text">${message}</span></div>`
 }
